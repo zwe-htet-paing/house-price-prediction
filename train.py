614ec9eaa93b186e7c511ef835d987b2843ae972
zwe-htet-paing <zwehtetpaing.works@gmail.com>
2024-11-19 17:38:03 +0700

    Update code

--- train.py
+++ train.py
@@ -73,7 +73,16 @@ def load_data(file_path: str) -> pd.DataFrame:
     return df
 
 def split_data(df: pd.DataFrame):
-    # Split data into train and test sets
+    """
+    Split data into train and test sets.
+
+    Parameters:
+    - df (pd.DataFrame): The data to be split.
+
+    Returns:
+    - X_train, X_test, y_train, y_test: The train and test sets split from the given DataFrame.
+    """
+    
     X = df.drop('house_price', axis=1)
     y = df['house_price']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
